237a2822487f8319b4f2888358ae6fe6cfe24f40
Raminx524 <Raminx524@Gmail.com>
2024-06-30 15:58:52 +0300

    Added user functionality

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const cors = require("cors");
 const connectDB = require("./config/db");
+const { verifyToken } = require("./middlewares/auth.middleware");
 
 async function main() {
   await connectDB();
@@ -12,6 +13,12 @@ async function main() {
       origin: "http://localhost:5173",
     })
   );
+
+  const authRoutes = require("./Routes/auth.route");
+  const protectedRoutes = require("./Routes/protected.route");
+
+  app.use("/api/auth", authRoutes);
+  app.use("/api/protected", verifyToken, protectedRoutes);
 
   const productRoutes = require("./Routes/product.route");
   app.use("/api/product", productRoutes);
