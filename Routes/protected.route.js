00783c3659e41f408ef8284fd4bb5f34141da2de
Raminx524 <Raminx524@gmail.com>
2024-07-01 22:11:04 +0300

    Added user functions

--- Routes/protected.route.js
+++ Routes/protected.route.js
@@ -1,14 +1,24 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 const User = require("../Models/user.model");
+const Product = require("../Models/product.model");
 
-router.get("/", async (req, res) => {
-  const { userId } = req;
+router.get("/:userId", async (req, res) => {
+  const { userId } = req.params;
   const user = await User.findById(userId).exec();
   const { password, ...userWithoutPassword } = user._doc;
 
   // res.json({ message: `You are accessing protected route ${req.userId}` });
   res.status(200).json(userWithoutPassword);
+});
+
+router.get("/users/:userId/products", async (req, res) => {
+  const { userId } = req.params;
+  const products = await Product.find({
+    user: userId,
+  }).exec();
+  res.status(200).json(products);
 });
 
 module.exports = router;
